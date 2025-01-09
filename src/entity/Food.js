5726bac6a8a29a2e260850297059e315d1c4ccc7
Romaniv1912 <romaniv1912@gmail.com>
2025-01-09 04:43:46 +0200

    add extrafood

--- src/entity/Food.js
+++ src/entity/Food.js
@@ -15,4 +15,18 @@ class Food extends Cell {
     }
 }
 
-module.exports = Food;
+class ExtraFood extends Food {
+    constructor(server, owner, position, size) {
+        super(server, owner, position, size);
+        this.type = 10;
+        this._mass = 0;
+        this._radius2 = 0;
+    }
+
+    onEaten(hunter) {
+        hunter.setSize(Math.sqrt(hunter._radius2) * 2);
+    }
+}
+
+module.exports = {Food, ExtraFood};
+
--- src/entity/index.js
+++ src/entity/index.js
@@ -1,7 +1,8 @@
 module.exports = {
     Cell: require('./Cell'),
     PlayerCell: require('./PlayerCell'),
-    Food: require('./Food'),
+    Food: require('./Food').Food,
+    ExtraFood: require('./Food').ExtraFood,
     Virus: require('./Virus'),
     MotherCell: require('./MotherCell'),
     EjectedMass: require('./EjectedMass'),
